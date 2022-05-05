Cifar with aug, fix nll calculation

dataloaders/base.py
@@ -397,12 +397,21 @@ def LSUN(dataroot, skip_normalization=False, train_aug=False):
 
     return train_dataset, train_dataset, resolution, 3
 
-def ImageNet(dataroot, skip_normalization=False, train_aug=False, resolution = 256):
+def ImageNet(dataroot, skip_normalization=False, train_aug=False, resolution = 64):
     dataset_dir = dataroot + "ImageNet/train"
     all_files = _list_image_files_recursively(dataset_dir)
     train_dataset = ImageDataset(image_paths=all_files, resolution=resolution, classes=np.zeros(len(all_files)))
     # dataset_dir = dataroot + "ImageNet/test"
     # all_files = _list_image_files_recursively(dataset_dir)
     # test_dataset = ImageDataset(image_paths=all_files, resolution=resolution, classes=np.zeros(len(all_files)))
+
+    return train_dataset, train_dataset, resolution, 3
+
+
+def CIFAR10AUG(dataroot, skip_normalization=False, train_aug=False):
+    dataset_dir = dataroot + "cifar_train/"
+    all_files = _list_image_files_recursively(dataset_dir)
+    resolution = 32
+    train_dataset = ImageDataset(image_paths=all_files, resolution=resolution, classes=np.zeros(len(all_files)))
 
     return train_dataset, train_dataset, resolution, 3

guided_diffusion/gaussian_diffusion.py
@@ -190,6 +190,7 @@ class GaussianDiffusion:
                 / (1.0 - self.alphas_cumprod)
         )
         self.dae_model = dae_model
+        self.calculate_nll = False
 
     def q_mean_variance(self, x_start, t):
         """
@@ -759,23 +760,28 @@ class GaussianDiffusion:
             true_mean, true_log_variance_clipped, out["mean"], out["log_variance"]
         )  # Calculate kl for all examples even with timestep=0 It will be overwritten later on
         kl = mean_flat(kl) / np.log(2.0)
-        if self.dae_model:
+        if self.dae_model and (not self.calculate_nll):
             # out["log_variance"] = th.zeros_like(out["log_variance"]) + self.constant_sigma * 2
-            decoder_mse = (out["mean"] - x_start) ** 2
-            decoder_loss_0 = mean_flat(decoder_mse)
-            if res_model_loss:
-                res_mse = 0
-                for batch in out["res_out"]:
-                    res_mse += (batch - x_start) ** 2
-                res_mse = mean_flat(res_mse)
-                decoder_loss_0+=res_mse
+            if self.loss_type == LossType.KL or self.loss_type == LossType.RESCALED_KL:
+                decoder_mse = (out["mean"] - x_start) ** 2
+                decoder_loss_0 = mean_flat(decoder_mse)
+                if res_model_loss:
+                    res_mse = 0
+                    for batch in out["res_out"]:
+                        res_mse += (batch - x_start) ** 2
+                    res_mse = mean_flat(res_mse)
+                    decoder_loss_0+=res_mse
+            else:
+                decoder_loss_0 = th.Tensor([0.0]).to(dist_util.dev())
         else:
+            if self.dae_model and self.calculate_nll:
+                out["log_variance"] = th.zeros_like(out["log_variance"]) + np.log(self.betas[0]) # 1e-1 #self.constant_sigma
             decoder_nll = -discretized_gaussian_log_likelihood(
                 x_start, means=out["mean"], log_scales=0.5 * out["log_variance"]
             )
             # print(th.exp(-0.5 * out["log_variance"][t==0]).mean().item())
             assert decoder_nll.shape == x_start.shape
-            decoder_loss_0 = mean_flat(decoder_nll) / np.log(2.0)
+            decoder_loss_0 =  mean_flat(decoder_nll) / np.log(2.0)
 
         # At the first timestep return the decoder NLL or MSE,
         # otherwise return KL(q(x_{t-1}|x_t,x_0) || p(x_{t-1}|x_t))
@@ -921,10 +927,16 @@ class GaussianDiffusion:
                     t=t,
                     clip_denoised=False,
                 )["output"]
+                if self.dae_model:
+                    terms["vb"][t == 0] = th.Tensor([0.0]).to(dist_util.dev())
                 if self.loss_type == LossType.RESCALED_MSE:
                     # Divide by 1000 for equivalence with initial implementation.
                     # Without a factor of 1/1000, the VB term hurts the MSE term.
-                    terms["vb"] *= self.num_timesteps / 1000.0
+                    # terms["vb"] *= self.num_timesteps / 1000.0
+                    if self.dae_model:
+                        terms["vb"] *= (self.num_timesteps - 1)/1000
+                    else:
+                        terms["vb"] *= self.num_timesteps/1000
 
             target = {
                 ModelMeanType.PREVIOUS_X: self.q_posterior_mean_variance(
@@ -935,6 +947,9 @@ class GaussianDiffusion:
             }[self.model_mean_type]
             assert model_output.shape == target.shape == x_start.shape
             terms["mse"] = mean_flat((target - model_output) ** 2)
+            # if self.dae_model:
+            #     mse_0_step = mean_flat((x_start - model_output) ** 2)
+            #     terms["mse"][t==0] = mse_0_step[t==0]
             if "vb" in terms:
                 terms["loss"] = terms["mse"] + terms["vb"]
             else:

scripts/image_nll.py
@@ -9,9 +9,13 @@ import numpy as np
 import torch.distributed as dist
 from dataloaders import base
 
+if os.uname().nodename == "titan4":
+    from guided_diffusion import dist_util_titan as dist_util
+else:
+    from guided_diffusion import dist_util
+
 from dataloaders.datasetGen import data_split
-from guided_diffusion import dist_util, logger
-from guided_diffusion.image_datasets import load_data
+from guided_diffusion import logger
 from guided_diffusion.script_util import (
     model_and_diffusion_defaults,
     create_model_and_diffusion,
@@ -61,10 +65,19 @@ def main():
     model, diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
+    diffusion.calculate_nll = True
     model_path = f"results/{args.experiment_name}/{args.model_path}"
-    model.load_state_dict(
-        dist_util.load_state_dict(model_path, map_location="cpu")
-    )
+    if args.model_name == "UNetModel":
+        model.load_state_dict(
+            dist_util.load_state_dict(model_path, map_location="cpu")
+        )
+    else:
+        model.unet_1.load_state_dict(
+            dist_util.load_state_dict(model_path + "_part_1.pt", map_location="cpu")
+        )
+        model.unet_2.load_state_dict(
+            dist_util.load_state_dict(model_path + "_part_2.pt", map_location="cpu")
+        )
     # if not os.environ.get("WANDB_MODE") == "disabled":
     #     wandb.watch(model, log_freq=10)
     model.to(dist_util.dev())
@@ -91,20 +104,23 @@ def main():
 
 
     logger.log("evaluating...")
-    diffusion.dae_model = False
+    # diffusion.dae_model = False
     for task_id in range(args.num_tasks):
         data = yielder(val_loaders[task_id])
-        run_bpd_evaluation(model, diffusion, data, args.num_samples, args.clip_denoised)
+        run_bpd_evaluation(model, diffusion, data, args.num_samples, args.clip_denoised, args.class_cond)
 
 
-def run_bpd_evaluation(model, diffusion, data, num_samples, clip_denoised):
+def run_bpd_evaluation(model, diffusion, data, num_samples, clip_denoised, class_cond):
     all_bpd = []
     all_metrics = {"vb": [], "mse": [], "xstart_mse": []}
     num_complete = 0
     while num_complete < num_samples:
         batch, model_kwargs = next(data)
         batch = batch.to(dist_util.dev())
-        model_kwargs = {k: v.to(dist_util.dev()) for k, v in model_kwargs.items()}
+        if class_cond:
+            model_kwargs = {k: v.to(dist_util.dev()) for k, v in model_kwargs.items()}
+        else:
+            model_kwargs = {}
         minibatch_metrics = diffusion.calc_bpd_loop(
             model, batch, clip_denoised=clip_denoised, model_kwargs=model_kwargs
         )

scripts/image_sample.py
@@ -46,6 +46,13 @@ def main():
         model.load_state_dict(
             dist_util.load_state_dict(args.model_path, map_location="cpu")
         )
+    elif args.model_name == "TwoPartsUNetModel":
+        model.unet_1.load_state_dict(
+            dist_util.load_state_dict(args.dae_path , map_location="cpu")
+        )
+        model.unet_2.load_state_dict(
+            dist_util.load_state_dict(args.model_path , map_location="cpu")
+        )
     else:
         model.unet_1.load_state_dict(
             dist_util.load_state_dict(args.model_path + "_part_1.pt", map_location="cpu")
@@ -93,19 +100,19 @@ def main():
             all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
         logger.log(f"created {len(all_images) * args.batch_size} samples")
 
-    arr = np.concatenate(all_images, axis=0)
-    arr = arr[: args.num_samples]
-    if args.class_cond:
-        label_arr = np.concatenate(all_labels, axis=0)
-        label_arr = label_arr[: args.num_samples]
-    if dist.get_rank() == 0:
-        shape_str = "x".join([str(x) for x in arr.shape])
-        out_path = os.path.join(logger.get_dir(), f"samples_{args.model_path.split('/')[-1][:-3]}.npz")
-        logger.log(f"saving to {out_path}")
+        arr = np.concatenate(all_images, axis=0)
+        arr = arr[: args.num_samples]
         if args.class_cond:
-            np.savez(out_path, arr, label_arr)
-        else:
-            np.savez(out_path, arr)
+            label_arr = np.concatenate(all_labels, axis=0)
+            label_arr = label_arr[: args.num_samples]
+        if dist.get_rank() == 0:
+            shape_str = "x".join([str(x) for x in arr.shape])
+            out_path = os.path.join(logger.get_dir(), f"samples_{args.model_path.split('/')[-1][:-3]}.npz")
+            logger.log(f"saving to {out_path}")
+            if args.class_cond:
+                np.savez(out_path, arr, label_arr)
+            else:
+                np.savez(out_path, arr)
 
     dist.barrier()
     plt.figure()
@@ -127,6 +134,7 @@ def create_argparser():
         model_path="",
         gpu_id=-1,
         num_tasks=1,
+        dae_path="",
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()